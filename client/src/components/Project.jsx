fe91bbfa8ec3f659e0c54055fba27f3470425643
Loloming <damianbruzzoni@gmail.com>
2024-07-13 19:54:27 -0300

    add: diseño responsive para el home

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -7,20 +7,20 @@ export default function Project({name, description, images, techs, code, demo}) 
     })
     return (
     <article
-        class="flex flex-row w-3/4 h-48 rounded-md border-l border-t border-r border-fuchsia-500/20 bg-gradient-to-br from-transparent via-violet-950/10 to-fuchsia-600/20 backdrop-blur-[2px]"
+        class="flex flex-row w-3/4 h-48 rounded-md border-l border-t border-r border-fuchsia-500/30 bg-gradient-to-br from-slate-500/10 via-violet-950/10 to-fuchsia-500/20 backdrop-blur-[2px] transition ease-in-out hover:scale-[1.02]"
     >
         <div class="w-2/4 mr-2">
             {!imageLoaded() && <div class="h-full w-full rounded-md bg-fuchsia-500/20 animate-pulse"></div>}
                 <img
                     src={images.src}
                     loading="eager"
-                    class={`${imageLoaded() ? 'block' : 'hidden'} w-full rounded-md h-full`}
+                    class={`${imageLoaded() ? 'block' : 'hidden'} z-0 w-full rounded-md h-full`}
                     alt={`Screenshot of my project ${name}`}
                 />
         </div>
         <div class="flex flex-col justify-evenly flex-wrap w-full h-full">
             <div class="flex flex-col w-full text-pretty gap-2">
-                <h5 class="mr-auto text-lg">{name}</h5>
+                <h5 class="mr-auto text-lg font-onest">{name}</h5>
                 <p class="text-sm">
                     {
                         (description && description) || (
@@ -34,7 +34,7 @@ export default function Project({name, description, images, techs, code, demo}) 
                     {
                         techs.map((t) => {
                             return (
-                                <li key={t} class="text-sm text-amber-200 p-1 h-min rounded-xl bg-gradient-to-br from-slate-500/10 to-fuchsia-500/20">
+                                <li key={t} class="text-sm font-onest font-normal text-amber-200 p-1 h-min rounded-xl bg-gradient-to-br from-slate-500/20 to-fuchsia-500/30 transition ease-in-out hover:-translate-y-[2px]">
                                     {t}
                                 </li>
                             );
@@ -42,10 +42,10 @@ export default function Project({name, description, images, techs, code, demo}) 
                     }
                 </ul>
                 <div
-                    class="flex flex-row text-white/50 justify-center gap-3 w-[25%]"
+                    class="flex flex-row text-white justify-center gap-3 w-[25%] font-onest"
                 >
-                    <a href={code}>Code</a>
-                    {demo && <a href={demo}>Demo</a>}
+                    <a class="h-min w-min bg-gradient-to-tr from-slate-500/10 via-violet-950/10 to-fuchsia-500/20 rounded-md mx-2" href={code}><h6 class="px-1">Code</h6></a>
+                    {demo && <a class="h-min w-min bg-gradient-to-tr from-slate-500/10 via-violet-950/10 to-fuchsia-500/20 rounded-md" href={demo}><h6 class="px-1">Demo</h6></a>}
                 </div>
             </div>
         </div>
--- client/tailwind.config.mjs
+++ client/tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+// import "@fontsource-variable/pixelify-sans";
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				pixel: ["Pixelify Sans Variable", "system-ui"],
+				onest: ["Onest Variable", "sans-serif"]
+			}
+		},
 	},
 	plugins: [],
 }

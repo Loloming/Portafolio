f130ae8f44f7fd26b5699cbee73ced09a32f1c75
Loloming <damianbruzzoni@gmail.com>
2024-07-14 20:30:57 -0300

    add: projects.jsx ahora es responsive, viewtransitions controladas

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -7,21 +7,21 @@ export default function Project({name, description, images, techs, code, demo}) 
     })
     return (
     <article
-        class="flex flex-row w-3/4 h-48 rounded-md border-l border-t border-r border-fuchsia-500/30 bg-gradient-to-br from-slate-500/10 via-violet-950/10 to-fuchsia-500/20 backdrop-blur-[2px] transition ease-in-out hover:scale-[1.02]"
+        class="flex lg:flex-row flex-col lg:h-48 h-min w-3/4 rounded-md border-l border-t border-r border-fuchsia-500/30 bg-gradient-to-br from-slate-500/10 via-violet-950/10 to-fuchsia-500/20 backdrop-blur-[2px] transition ease-in-out hover:scale-[1.02]"
     >
-        <div class="w-2/4 mr-2">
-            {!imageLoaded() && <div class="h-full w-full rounded-md bg-fuchsia-500/20 animate-pulse"></div>}
+        <div class="lg:w-2/4 lg:mr-2">
+            {!imageLoaded() && <div class="lg:h-full w-full xl:h-full md:h-52 h-40 lg:rounded-md rounded-t-md bg-fuchsia-500/20 animate-pulse"></div>}
                 <img
                     src={images.src}
                     loading="eager"
-                    class={`${imageLoaded() ? 'block' : 'hidden'} z-0 w-full rounded-md h-full`}
+                    class={`${imageLoaded() ? 'block' : 'hidden'} z-0 w-full lg:h-full md:h-52 h-40 lg:rounded-md rounded-t-md`}
                     alt={`Screenshot of my project ${name}`}
                 />
         </div>
-        <div class="flex flex-col justify-evenly flex-wrap w-full h-full">
+        <div class="flex flex-col lg:gap-0 lg:p-0 gap-2 p-2 justify-evenly flex-wrap w-full h-full">
             <div class="flex flex-col w-full text-pretty gap-2">
-                <h5 class="mr-auto text-lg font-onest">{name}</h5>
-                <p class="text-sm">
+                <h5 class="lg:mr-auto lg:mx-0 mx-auto xl:text-xl text-base font-onest font-medium">{name}</h5>
+                <p class="lg:text-base md:text-sm md:px-0 text-xs px-2">
                     {
                         (description && description) || (
                             <div class="animate-pulse bg-fuchsia-600/20 w-full h-[.8rem] rounded-full" />
@@ -29,12 +29,12 @@ export default function Project({name, description, images, techs, code, demo}) 
                     }
                 </p>
             </div>
-            <div class="flex flex-row justify-start w-full">
-                <ul class="flex flex-row flex-wrap justify-start gap-1 w-3/4">
+            <div class="flex flex-row md:justify-between justify-center w-full">
+                <ul class="flex flex-row flex-wrap md:items-center justify-start gap-1 w-3/4">
                     {
                         techs.map((t) => {
                             return (
-                                <li key={t} class="text-sm font-onest font-normal text-amber-200 p-1 h-min rounded-xl bg-gradient-to-br from-slate-500/20 to-fuchsia-500/30 transition ease-in-out hover:-translate-y-[2px]">
+                                <li key={t} class="lg:text-base sm:text-sm text-xs font-onest font-normal text-amber-200 p-1 h-min rounded-xl bg-gradient-to-br from-slate-500/20 to-fuchsia-500/30 transition ease-in-out hover:-translate-y-[2px]">
                                     {t}
                                 </li>
                             );
@@ -42,10 +42,10 @@ export default function Project({name, description, images, techs, code, demo}) 
                     }
                 </ul>
                 <div
-                    class="flex flex-row text-white justify-center gap-3 w-[25%] font-onest"
+                    class="flex md:flex-row flex-col items-center text-white lg:justify-center gap-3 lg:w-[25%] w-min lg:text-base sm:text-sm text-xs font-onest"
                 >
-                    <a class="h-min w-min bg-gradient-to-tr from-slate-500/10 via-violet-950/10 to-fuchsia-500/20 rounded-md mx-2" href={code}><h6 class="px-1">Code</h6></a>
-                    {demo && <a class="h-min w-min bg-gradient-to-tr from-slate-500/10 via-violet-950/10 to-fuchsia-500/20 rounded-md" href={demo}><h6 class="px-1">Demo</h6></a>}
+                    <a class="h-min w-max bg-gradient-to-tr from-slate-500/10 via-violet-950/10 to-fuchsia-500/20 rounded-md mx-2" href={code}><h6 class="p-1">Code</h6></a>
+                    {demo && <a class="h-min w-max bg-gradient-to-tr from-slate-500/10 via-violet-950/10 to-fuchsia-500/20 rounded-md" href={demo}><h6 class="p-1">Demo</h6></a>}
                 </div>
             </div>
         </div>
